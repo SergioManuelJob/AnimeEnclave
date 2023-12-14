c153bf86978c5ff74c8bc8a419e0b03b4e7b798f
SergioManuelJob <suarezsergiomanuel69@gmail.com>
2023-12-14 18:35:52 +0000

    Login page styles done with

--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -7,9 +7,9 @@ function Login() {
             <img src="/logo.png" alt="Logo of the page, a slime"/>
             <h1>AnimeEnclave</h1>
             <label>Username</label>
-            <input type='text' placeholder='ExampleUser...'></input>
+            <input type='text' placeholder='i.e: My user'></input>
             <label>Password</label>
-            <input type='text' placeholder='Password123...'></input>
+            <input type='text' placeholder='i.e: Password123'></input>
 
             <button className='loginButton'>Log In!</button>
         </form>
